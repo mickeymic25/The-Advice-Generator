c156541d17eea465d6cd5cab73ba76af2d17f424
mickeymic25 <mopokumensah18@gmail.com>
2024-01-03 22:41:54 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const generator_url = "https://api.adviceslip.com/advice";
+const btn = document.getElementById("btn");
+const getAdvice = async () => {
+  try {
+    const response = await fetch(generator_url);
+    if (response.ok) {
+      const data = await response.json();
+
+      const data_displayed = data.slip.advice;
+      const data_id = data.slip.id;
+      document.querySelector(".advice-id").innerHTML = `Advice ${data_id}`;
+      document.querySelector(".advice-text").innerHTML = data_displayed;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  getAdvice();
+});
+btn.addEventListener("click", () => {
+  //alert("Button pressed, please wait for result..");
+  getAdvice();
+});
